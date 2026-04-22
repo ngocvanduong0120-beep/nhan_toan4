f7d6f432d92a588fcbe014e96a2b84c8315289ab
van <ngocvan.duong0120@gmail.com>
2026-04-22 21:16:51 +0700

    Replace files in destination

---
+++ html5/story_content/triggers.js
@@ -0,0 +1,815 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6oTL1hOCFdL":
+        Script1();
+        break;
+      case "5talWFhZIgp":
+        Script2();
+        break;
+      case "6grrykJ24Bg":
+        Script3();
+        break;
+      case "66gv5sigj4u":
+        Script4();
+        break;
+      case "5y9RO4n3ugy":
+        Script5();
+        break;
+      case "6gQokhaaTT6":
+        Script6();
+        break;
+      case "6BPOJmG329B":
+        Script7();
+        break;
+      case "5pK7tt29idX":
+        Script8();
+        break;
+      case "5mpIzzVSfhq":
+        Script9();
+        break;
+      case "6Cs9nilpsmb":
+        Script10();
+        break;
+      case "6DLKEPu492p":
+        Script11();
+        break;
+      case "5Xu1Ffmtlqr":
+        Script12();
+        break;
+      case "5bHZxzZ0Ja1":
+        Script13();
+        break;
+      case "6Z4kwfAwjKB":
+        Script14();
+        break;
+      case "5o4dgwO0qRK":
+        Script15();
+        break;
+      case "6QQALhOvb3y":
+        Script16();
+        break;
+      case "5yOrRTx07bI":
+        Script17();
+        break;
+      case "5yUIe3S2bdR":
+        Script18();
+        break;
+      case "67R7QsV4Mze":
+        Script19();
+        break;
+      case "5bS2XjHkb2r":
+        Script20();
+        break;
+      case "5qSJZiFLTPs":
+        Script21();
+        break;
+      case "5eLFVZpDyga":
+        Script22();
+        break;
+      case "5jVDQIS7AT5":
+        Script23();
+        break;
+      case "6mH0QD7Qiil":
+        Script24();
+        break;
+      case "6kBwFaDW5QS":
+        Script25();
+        break;
+      case "5iI07eMbOQA":
+        Script26();
+        break;
+      case "5yNoNjJ5kky":
+        Script27();
+        break;
+      case "6mwj7IfxxQ7":
+        Script28();
+        break;
+      case "5caYhQvCDcU":
+        Script29();
+        break;
+      case "6PKUuR97Cx7":
+        Script30();
+        break;
+      case "61SB5i6wbxI":
+        Script31();
+        break;
+      case "6MjF6URYHg1":
+        Script32();
+        break;
+      case "6DnfkryBnfb":
+        Script33();
+        break;
+      case "5zLX708mcvP":
+        Script34();
+        break;
+      case "6QtNxpvVveA":
+        Script35();
+        break;
+      case "5pUCWjN53Iy":
+        Script36();
+        break;
+      case "6KGoMlUBjGa":
+        Script37();
+        break;
+      case "5e2MWCVLYZw":
+        Script38();
+        break;
+      case "6ieiCBdy9fp":
+        Script39();
+        break;
+      case "6SCq3tY4xv7":
+        Script40();
+        break;
+      case "6KhXbgPlGju":
+        Script41();
+        break;
+      case "6CDnWFGyQ1O":
+        Script42();
+        break;
+      case "5bXXSS5o26m":
+        Script43();
+        break;
+      case "6W3bxU2xo3I":
+        Script44();
+        break;
+      case "66SUZ263lCK":
+        Script45();
+        break;
+      case "5eE7leez42l":
+        Script46();
+        break;
+      case "6QcnuofTpp8":
+        Script47();
+        break;
+      case "68deBVOQARh":
+        Script48();
+        break;
+      case "6FxPJoSZlEW":
+        Script49();
+        break;
+      case "60LyrRHNiJp":
+        Script50();
+        break;
+      case "6IM7lQ1R5JA":
+        Script51();
+        break;
+      case "6jQ1UhPaqQc":
+        Script52();
+        break;
+      case "5uVEQvlfqM1":
+        Script53();
+        break;
+      case "6cjKZhYvbg1":
+        Script54();
+        break;
+      case "6bvIJcDRApU":
+        Script55();
+        break;
+      case "6PQP6Bu9Fcz":
+        Script56();
+        break;
+      case "6P8oSmDHoiV":
+        Script57();
+        break;
+      case "6TN8WBYMGsU":
+        Script58();
+        break;
+      case "6l90s23aDdY":
+        Script59();
+        break;
+      case "6fBO2uUqmiW":
+        Script60();
+        break;
+      case "5hivhd8xNWX":
+        Script61();
+        break;
+      case "6fxuvpajUoH":
+        Script62();
+        break;
+      case "613KzE9ECfR":
+        Script63();
+        break;
+      case "6c53pmACt7C":
+        Script64();
+        break;
+      case "6V9t61gUXDN":
+        Script65();
+        break;
+      case "6SQ8Zzv0iww":
+        Script66();
+        break;
+      case "5kT8mSh7CnM":
+        Script67();
+        break;
+      case "6Jujz6a36DI":
+        Script68();
+        break;
+      case "5om9mKgdOz1":
+        Script69();
+        break;
+      case "5w45LUCqZim":
+        Script70();
+        break;
+      case "6KP0skaVe5J":
+        Script71();
+        break;
+      case "611jU4QPV5D":
+        Script72();
+        break;
+      case "6lXMt6RfOc7":
+        Script73();
+        break;
+      case "5gZbdO9WViz":
+        Script74();
+        break;
+      case "6daEi9T11jl":
+        Script75();
+        break;
+      case "6l2f08Pt2Xo":
+        Script76();
+        break;
+      case "5r8CVLU0ja3":
+        Script77();
+        break;
+      case "5eEFoDchjan":
+        Script78();
+        break;
+      case "6mVlLIT1SrH":
+        Script79();
+        break;
+      case "5pL2pUc8dnF":
+        Script80();
+        break;
+      case "5qyRHipahTw":
+        Script81();
+        break;
+      case "5pWIPZlxNuT":
+        Script82();
+        break;
+      case "5coLwhsW4tW":
+        Script83();
+        break;
+      case "5lGA0qbEVix":
+        Script84();
+        break;
+      case "6MAterZEm1f":
+        Script85();
+        break;
+      case "6OFtkdTCVJA":
+        Script86();
+        break;
+      case "63TOrxb8C4X":
+        Script87();
+        break;
+      case "6g2lfCbpPeg":
+        Script88();
+        break;
+      case "6ZVo0QgKUFb":
+        Script89();
+        break;
+      case "6p21I5Is7iT":
+        Script90();
+        break;
+      case "5iixYzLG1KB":
+        Script91();
+        break;
+      case "5ZTvpyJcVIT":
+        Script92();
+        break;
+      case "5lQU5yZy8Ly":
+        Script93();
+        break;
+      case "6npRsnSkxfq":
+        Script94();
+        break;
+      case "6GB509g5idb":
+        Script95();
+        break;
+      case "5yevOI6iA1g":
+        Script96();
+        break;
+      case "6oS4M17NprO":
+        Script97();
+        break;
+      case "5jPW3A7EKCa":
+        Script98();
+        break;
+      case "6pQmysgBldH":
+        Script99();
+        break;
+      case "6QEpD8NsXtz":
+        Script100();
+        break;
+      case "6hePKcsw2Dr":
+        Script101();
+        break;
+      case "5bb1d122sDC":
+        Script102();
+        break;
+      case "5ofr3GgyZNQ":
+        Script103();
+        break;
+      case "6SRIIoaA8Mm":
+        Script104();
+        break;
+      case "62UN4Noy7Ys":
+        Script105();
+        break;
+      case "5zG1WMe4esT":
+        Script106();
+        break;
+      case "5rOsDWUFIIU":
+        Script107();
+        break;
+      case "60jwFeyAdBJ":
+        Script108();
+        break;
+      case "68tMPzQeKKi":
+        Script109();
+        break;
+      case "64eLiNWbJCa":
+        Script110();
+        break;
+      case "6MVgf8bbAhk":
+        Script111();
+        break;
+      case "63armFi0kHO":
+        Script112();
+        break;
+      case "5lZthLNOv1e":
+        Script113();
+        break;
+      case "5gknFTQhu9H":
+        Script114();
+        break;
+      case "5vXIRaZx5Yf":
+        Script115();
+        break;
+      case "688l6MXXwrJ":
+        Script116();
+        break;
+      case "6SZT6e5y8cH":
+        Script117();
+        break;
+      case "5s7boB3Ayw2":
+        Script118();
+        break;
+      case "6Ew9pp25DIP":
+        Script119();
+        break;
+      case "6OefDrCMu53":
+        Script120();
+        break;
+      case "5ln5ExJJYRd":
+        Script121();
+        break;
+      case "6QBqiJY38k4":
+        Script122();
+        break;
+      case "6F7nAVJ1yCY":
+        Script123();
+        break;
+      case "5lUup0U4dC0":
+        Script124();
+        break;
+      case "5lAasOk4iPi":
+        Script125();
+        break;
+      case "6ormF9xtTdR":
+        Script126();
+        break;
+      case "6MYtcDJThie":
+        Script127();
+        break;
+      case "5w5QMzBxRfp":
+        Script128();
+        break;
+      case "5lgoIx2ewgg":
+        Script129();
+        break;
+      case "6eGVbOzMK6q":
+        Script130();
+        break;
+      case "5XwCFBcLmYx":
+        Script131();
+        break;
+      case "5jP9hVnCsxi":
+        Script132();
+        break;
+      case "5fw3kwhJE0I":
+        Script133();
+        break;
+      case "5v6rkrw57Nz":
+        Script134();
+        break;
+      case "6bAZBY1ufSW":
+        Script135();
+        break;
+      case "6Zd7gBYVgvr":
+        Script136();
+        break;
+      case "5zNydBaWt6B":
+        Script137();
+        break;
+      case "60u2INiPp5U":
+        Script138();
+        break;
+      case "6qYjq3jhrCB":
+        Script139();
+        break;
+      case "6JFDdAyCKHV":
+        Script140();
+        break;
+      case "5qIfs2DeLlX":
+        Script141();
+        break;
+      case "6JAYiTeeVEu":
+        Script142();
+        break;
+      case "5tJmPWnLTXd":
+        Script143();
+        break;
+      case "6qVN7UY1ydU":
+        Script144();
+        break;
+      case "5fdSgq3883s":
+        Script145();
+        break;
+      case "6oHBDxoZpm4":
+        Script146();
+        break;
+      case "6IK2k5VkA5l":
+        Script147();
+        break;
+      case "6HwnYILjd8D":
+        Script148();
+        break;
+      case "5mYvOAYinsy":
+        Script149();
+        break;
+      case "6VtmAPaPz3v":
+        Script150();
+        break;
+      case "6A5UWJiuIKV":
+        Script151();
+        break;
+      case "6B8eNMHbepY":
+        Script152();
+        break;
+      case "5zDqLpbsUSY":
+        Script153();
+        break;
+      case "5pdSSQnYRGW":
+        Script154();
+        break;
+      case "6KJjOS6VAKp":
+        Script155();
+        break;
+      case "6YrVX4kVSeX":
+        Script156();
+        break;
+      case "6Ykrb58T515":
+        Script157();
+        break;
+      case "6Vetl6aJkJs":
+        Script158();
+        break;
+      case "66oAUTWJ8SQ":
+        Script159();
+        break;
+      case "5m6D5H4ldhL":
+        Script160();
+        break;
+      case "6qZYkkKP2kS":
+        Script161();
+        break;
+      case "67yVvc0vJLB":
+        Script162();
+        break;
+      case "5fF89b8p4lH":
+        Script163();
+        break;
+      case "5hRhTSiM4JL":
+        Script164();
+        break;
+      case "6fCqExE1X30":
+        Script165();
+        break;
+      case "60DZXkGC5B5":
+        Script166();
+        break;
+      case "6S0O3keTgCM":
+        Script167();
+        break;
+      case "6eAsMS1nmPZ":
+        Script168();
+        break;
+      case "69Eqtr8uZG1":
+        Script169();
+        break;
+      case "5rcoXGTWA3B":
+        Script170();
+        break;
+      case "5XKkAp2z8vS":
+        Script171();
+        break;
+      case "664hWMvkKGf":
+        Script172();
+        break;
+      case "5WZV6JwOOrf":
+        Script173();
+        break;
+      case "6b1QtKmQsPU":
+        Script174();
+        break;
+      case "5cPCXzdi1T4":
+        Script175();
+        break;
+      case "6JMVPNN0fOr":
+        Script176();
+        break;
+      case "5Vg1AuRBedD":
+        Script177();
+        break;
+      case "63UljMCY1WS":
+        Script178();
+        break;
+      case "5X9chk4jX91":
+        Script179();
+        break;
+      case "5xssvRGx2KC":
+        Script180();
+        break;
+      case "6XrA4w3sG5r":
+        Script181();
+        break;
+      case "6QJ24BzsVYR":
+        Script182();
+        break;
+      case "63CzU2kvDel":
+        Script183();
+        break;
+      case "5kyqFLRJUBv":
+        Script184();
+        break;
+      case "6X8WPUtQ4sl":
+        Script185();
+        break;
+      case "65u8XP9eX3F":
+        Script186();
+        break;
+      case "5vsVzPLa6xs":
+        Script187();
+        break;
+      case "6IbAxq6EDQe":
+        Script188();
+        break;
+      case "5mI7QeA6WGe":
+        Script189();
+        break;
+      case "6auBsqUWb91":
+        Script190();
+        break;
+      case "6hIO9z77cfJ":
+        Script191();
+        break;
+      case "6MPGRQpGPpw":
+        Script192();
+        break;
+      case "6OUZuwHOaSx":
+        Script193();
+        break;
+      case "63PuBrFgd4D":
+        Script194();
+        break;
+      case "60kB9vQ5hiq":
+        Script195();
+        break;
+      case "6AhXF61lIul":
+        Script196();
+        break;
+      case "6iEtbMdFbV5":
+        Script197();
+        break;
+      case "5yqiUHVoE35":
+        Script198();
+        break;
+      case "62MD8dFgwGF":
+        Script199();
+        break;
+      case "5x6tk8t2zoq":
+        Script200();
+        break;
+      case "5ttMrO22MXf":
+        Script201();
+        break;
+      case "6GxemgeTyuz":
+        Script202();
+        break;
+      case "6hRB4DUj3yV":
+        Script203();
+        break;
+      case "68xivP0Pty7":
+        Script204();
+        break;
+      case "6Gbk2ZZhZRb":
+        Script205();
+        break;
+      case "6n4YlPpqCca":
+        Script206();
+        break;
+      case "6kbX1u0PXxq":
+        Script207();
+        break;
+      case "6qcEnR7LDhW":
+        Script208();
+        break;
+      case "5uyigZFKgfp":
+        Script209();
+        break;
+      case "6jFbavtAK2n":
+        Script210();
+        break;
+      case "6LKVH805H7u":
+        Script211();
+        break;
+      case "61Pmz5MZ0sw":
+        Script212();
+        break;
+      case "6gb7KILgd7o":
+        Script213();
+        break;
+      case "5zDli0OKKCI":
+        Script214();
+        break;
+      case "5bE5SKTD3u3":
+        Script215();
+        break;
+      case "6HccapW08CF":
+        Script216();
+        break;
+      case "6YT15QE7fPF":
+        Script217();
+        break;
+      case "6D9m0EEMIdl":
+        Script218();
+        break;
+      case "5qK9AyQ8Vr7":
+        Script219();
+        break;
+      case "5ae8UUCu8LY":
+        Script220();
+        break;
+      case "6R8nhOpsN22":
+        Script221();
+        break;
+      case "5oYtxNcO089":
+        Script222();
+        break;
+      case "6RdoSgyUs9v":
+        Script223();
+        break;
+      case "5eDkqmnlk9A":
+        Script224();
+        break;
+      case "6e96qUtbAhN":
+        Script225();
+        break;
+      case "5Y8MOqKJOfL":
+        Script226();
+        break;
+      case "6o91kKpwo5F":
+        Script227();
+        break;
+      case "634XenHrZ6o":
+        Script228();
+        break;
+      case "5dIl2oA3Spr":
+        Script229();
+        break;
+      case "6NR3CjTbNhf":
+        Script230();
+        break;
+      case "6q4kaQkSOxo":
+        Script231();
+        break;
+      case "6kSC0PDUmRC":
+        Script232();
+        break;
+      case "6GpQxiJ4h5w":
+        Script233();
+        break;
+      case "5eO0OEEJNXF":
+        Script234();
+        break;
+      case "6EiuXejU9Wz":
+        Script235();
+        break;
+      case "6LPcaj0ddOw":
+        Script236();
+        break;
+      case "6fiGUU2flJF":
+        Script237();
+        break;
+      case "5vkpFY2pqrS":
+        Script238();
+        break;
+      case "6Ex9dIx2Mw3":
+        Script239();
+        break;
+      case "63oS0ESwOTS":
+        Script240();
+        break;
+      case "5mOgVOWHHMn":
+        Script241();
+        break;
+      case "644QjJWfkfX":
+        Script242();
+        break;
+      case "5tdQQqf3zVZ":
+        Script243();
+        break;
+      case "5deUGi91guT":
+        Script244();
+        break;
+      case "6mpJe0nh3k7":
+        Script245();
+        break;
+      case "6mfcoop1Vv8":
+        Script246();
+        break;
+      case "6Xv1eZ675qR":
+        Script247();
+        break;
+      case "6UazjZOksXE":
+        Script248();
+        break;
+      case "5YldXtANz1m":
+        Script249();
+        break;
+      case "6aEKyFtlJ4B":
+        Script250();
+        break;
+      case "5jYpPYYKz9e":
+        Script251();
+        break;
+      case "6iKLCSJqNyQ":
+        Script252();
+        break;
+      case "6L8AH5BEVEb":
+        Script253();
+        break;
+      case "6i4tM8ZOod7":
+        Script254();
+        break;
+      case "5enToR9mqHC":
+        Script255();
+        break;
+      case "5umfv8ayi9G":
+        Script256();
+        break;
+      case "5bO7IFzbIPx":
+        Script257();
+        break;
+      case "6dQaMTfLMju":
+        Script258();
+        break;
+      case "6AEiQ4RogO6":
+        Script259();
+        break;
+      case "5YXV3DbFFob":
+        Script260();
+        break;
+      case "5pqi6GOeIVn":
+        Script261();
+        break;
+      case "5ZI8InlCLhI":
+        Script262();
+        break;
+      case "5Wama9Obdfc":
+        Script263();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var once = player.once;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+var update = player.update;
+var pointerX = player.pointerX;
+var pointerY = player.pointerY;
+var showPointer = player.showPointer;
+var hidePointer = player.hidePointer;
+var slideWidth = player.slideWidth;
+var slideHeight = player.slideHeight;
+var getKeyDown = player.getKeyDown;
+var keydown = player.keydown;
+var keyup = player.keyup;
+};
